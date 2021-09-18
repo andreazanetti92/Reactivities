76b3c919eaa8b106acd77f8d4da687b322535115
Andrea Zanetti <andrea.zanetti@stud.tecnicosuperiorekennedy.it>
2021-09-18 22:13:45 +0200

    Added Client React, CORS, ...

---
+++ client-app/src/App.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
+
+type Activity = {
+    id: string,
+    title: string,
+    date: Date,
+    description: string,
+    city: string,
+    venue: string,
+    category: string
+}
+
+function App() {
+
+    const [activities, setActivities] = useState([]);
+
+    useEffect(() => {
+        axios.get('https:localhost:5001/api/activities').then((res) => {
+            console.log(res);
+            setActivities(res.data);
+        })
+    }, [])
+
+  return (
+      <div>
+          <Header as="h2" icon="users" content="Reactivities" />
+          
+              <List>
+                  {activities.map((activity: Activity) => 
+                      <List.Item key={activity.id}>
+                        {activity.title}
+                      </List.Item>
+                  )}
+
+              </List>
+    </div>
+  );
+}
+
+export default App;
